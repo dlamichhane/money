6c4f86f98a27182163eabe4d82638657133cf8a6
Deepak Lamichhane <dlamichhane@hotmail.com>
2015-09-27 22:16:27 +0200

    last-rspec-test-as-a-completion

--- spec/comparable_spec.rb
+++ spec/comparable_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe 'Comparable' do
-  let(:exchange_states) {Money.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoin' => 0.0047})}
+  subject {Money.conversion_rates('EUR', {'USD' => 1.11, 'Bitcoin' => 0.0047})}
   
   before do 
     @fifty_euro = Money.new(50, 'EUR')
@@ -9,8 +9,29 @@ describe 'Comparable' do
   end
 
   describe '#==' do
-    it 'should' do
-      true
+    it 'should return true of same currency and equal amount' do
+      (@twenty_dollars == Money.new(20, 'USD')).should be true
+    end
+
+    it 'should return false of same currency and not equal amount' do
+      (@twenty_dollars == Money.new(30, 'USD')).should be false
+    end
+
+    it 'should return true of same currency and equal amount' do
+      fifty_eur_in_usd = "55.50 USD"
+      (@fifty_euro == fifty_eur_in_usd).should be true
+    end
+  end
+
+  describe '#>' do
+    it 'should return true if first amount greater than second amount of same currency' do
+      (@twenty_dollars > Money.new(5, 'USD')).should be true
+    end
+  end
+
+  describe '#<' do
+    it 'should return true if first amount greater than second amount of same currency' do
+      (@twenty_dollars < @fifty_euro).should be true
     end
   end
   
